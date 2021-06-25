6e7b2a2981ab19a4cd494cad5c1c916cb1e9cbe4
Benjamín de Jesús Pérez Aguilar <benjaminperez201831504@cunoc.edu.gt>
2021-06-24 21:37:17 -0600

    fue relizado todo los archivos del primer boceto

---
+++ src/main/java/com/mycompany/escaleraserpiente/Start.java
@@ -0,0 +1,17 @@
+package com.mycompany.escaleraserpiente;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author drymnz
+ */
+public class Start {
+    public static void main(String[] args) {
+        
+    }
+}

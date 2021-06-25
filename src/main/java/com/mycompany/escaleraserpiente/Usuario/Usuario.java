aa030c5a8bc40d8394c83e116fdc4582955a9466
Benjamin de Jesus Perez Aguilar <benjaminperez201831504@cunoc.edu.gt>
2021-06-24 22:53:43 -0600

    fue creado la clase usuario con las indicaciones del proyecto

--- src/main/java/com/mycompany/escaleraserpiente/Usuario/Usuario.java
+++ src/main/java/com/mycompany/escaleraserpiente/Usuario/Usuario.java
@@ -10,5 +10,24 @@ package com.mycompany.escaleraserpiente.Usuario;
  * @author drymnz
  */
 public class Usuario {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private int cantidadPartidad;
+    private int perdida;
+    private int victoria;
+    // construtores
+    public Usuario (int id,String nombre,String apellido, int cantidadPartidad,int perdida,int victoria) {
+        this.id =  id;
+        this.nombre =  nombre;
+        this.apellido =  apellido;
+        this.cantidadPartidad =  cantidadPartidad;
+        this.perdida =  perdida;
+        this.victoria =  victoria;
+    }
+    public Usuario (int id,String nombre,String apellido) {
+        this(id, nombre, apellido, 0, 0, 0);
+    }
+    //fin construtores
     
 }
